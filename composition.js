6644d059a6a3fc4366987fe1c36da9af22306cf8
Rowan-James Tran <rj@ThinkPadT61.(none)>
2013-07-02 20:43:07 -0700

    f

--- composition.js
+++ composition.js
@@ -3,19 +3,19 @@ var displayComposition = function(molecule) {
   $("#image").text("");
   if (molecule == "salt") {
     var text = " <b>Table salt</b>";
-    $("#image").append("<img src='salt.png'>");
+    $("#image").prepend("<img src='salt.png'>");
   } else if (molecule == "water") {
     var text = " <b>Water</b>";
-    $("#image").append("<img src='water.png'>");
+    $("#image").prepend("<img src='water.png'>");
   } else if (molecule == "co2") {
     var text = " <b>Carbon dioxide</b>";
-    $("#image").append("<img src='co2.png'>");
+    $("#image").prepend("<img src='co2.png'>");
   } else if (molecule == "oxygen") {
     var text = " <b>Oxygen</b>";
-    $("#image").append("<img src='oxygen.png'>");
+    $("#image").prepend("<img src='oxygen.png'>");
   } else {
     var text = " <b>Deoxyribonucleic acid</b>";
-    $("#image").append("<img src='dna.png'>");
+    $("#image").prepend("<img src='dna.png'>");
   }
   $("#currentview").append(text);
 };
